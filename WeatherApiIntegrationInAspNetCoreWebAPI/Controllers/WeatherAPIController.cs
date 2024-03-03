34318eba87810c03c4b33c82006a91e594becfce
mudassarali.official <mudassarali.official@gmail.com>
2024-03-04 03:30:41 +0500

    2007-WeatherAPIController Code Clean Up

--- WeatherApiIntegrationInAspNetCoreWebAPI/Controllers/WeatherAPIController.cs
+++ WeatherApiIntegrationInAspNetCoreWebAPI/Controllers/WeatherAPIController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WeatherApi.Core.Interfaces;
 
 namespace WeatherApiIntegrationInAspNetCoreWebAPI.Controllers
